203a64d89f501ac3b888e6e5f7e6d6518c1bad65
akbarakma1234 <akbarajo1234@gmail.com>
2023-06-28 14:37:07 +0700

    init

---
+++ src/data/user.js
@@ -0,0 +1,63 @@
+const INFO = {
+	main: {
+		title: "Akbar Akma Portofolio",
+		name: "Akbar Danial Akma",
+		email: "akbarajo1234@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/akbarakma",
+		linkedin: "https://www.linkedin.com/in/akbarakma",
+		cv: "https://github.com/akbarakma",
+	},
+
+	homepage: {
+		title: "Full-Stack Developer with Backend Specialization",
+		description:
+			"My name is Akbar Danial Akma. I am a backend developer with expertise in Node.js and Typescript. I have experience in building applications from scratch and collaborating with larger teams.",
+	},
+
+	about: {
+		title: "I'm Akbar Danial Akma. I live in Jakarta, Indonesia, where I code for a living.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Resilient and fast-learning backend specialist, experienced in building applications from scratch and collaborating with larger teams. Passionate about delivering efficient and scalable solutions, Committed to continuous growth, I consistently achieve impactful results.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "BattleShip Online Game",
+			description:
+				"I developed an online Battleship game using React.js and Socket.io. This project allows players to enjoy the classic board game experience in an online environment. Engage in PvP battles and test your strategic skills against opponents in real-time.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://battleship-game.akbarakma.site",
+		},
+
+		{
+			title: "Grow With KG Website",
+			description:
+				"Collaborated with a company to create and deploy their website profile using Firebase and DigitalOcean, starting the project from scratch and implemented PostgreSQL as the backend database solution for the CMS admin endpoint",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://growwithkg.id/",
+		},
+
+		{
+			title: "Simple BlackJack Game",
+			description:
+				"Made a simple blackjack game using HTML, CSS, and Javascript.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://akbarakma.github.io/blackjack",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Things I’ve made on my spare time while working fulltime.
+						</div>
+
+						<div className="subtitle projects-subtitle" style={{ color: 'black' }}>
+						In my spare time while working full-time, 
+						I have embarked on a range of solo projects 
+						that I take great pride in. From developing an 
+						online Battleship game using React.js and TypeScript 
+						to various other diverse creations, these passion projects 
+						showcase my dedication and expertise. I welcome suggestions, 
+						feedback, or new ideas, so feel free to reach out to me and 
+						let's collaborate on exciting endeavors.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
